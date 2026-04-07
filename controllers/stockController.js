6f451f4223ec3ba3bcfa63058ab7358daaf9a5a7
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-04-07 05:46:23 +0530

    adding controller to fetch and return stock data by symbol

---
+++ controllers/stockController.js
@@ -0,0 +1,23 @@
+
+const { fetchStockData } = require('../services/stockService');
+
+const getStockData = async (req, res) => {
+  try {
+    const { symbol } = req.params;
+    const data = await fetchStockData(symbol);
+
+    res.status(200).json({
+      success: true,
+      symbol,
+      data
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Failed to fetch stock data'
+    });
+  }
+};
+
+module.exports = { getStockData };
