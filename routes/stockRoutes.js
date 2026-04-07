8b32d6c9727c5ecc8930e11fe6f0c96cb9b56a31
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-04-07 05:48:05 +0530

    adding stock routes to handle symbol based API requests

---
+++ routes/stockRoutes.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const router = express.Router();
+const { getStockData } = require('../controllers/stockController');
+
+
+router.get('/:symbol', getStockData);
+
+module.exports = router;
