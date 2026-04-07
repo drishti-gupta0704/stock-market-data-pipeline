699f2fed0e6025796ea92b77ac162846e2cea9e8
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-04-07 06:02:43 +0530

    changing the port because it was showing access denied to the local port

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.get('/', (req, res) => {
   res.send('Stock Data Pipeline API is running');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
