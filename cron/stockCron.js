4b0b61e8c0fe4c6272f7082afeec9ab57a93a301
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-04-15 10:53:33 +0530

    adding cron job to schedule stock data fetching every 10 minutes

---
+++ cron/stockCron.js
@@ -0,0 +1,26 @@
+
+const cron = require('node-cron');
+const stockQueue = require('../queues/stockQueue');
+
+const symbols = ['AAPL', 'TSLA', 'GOOGL'];
+
+cron.schedule('*/10 * * * *', async () => {
+  console.log('Running stock cron job...');
+
+  try {
+    for (let symbol of symbols) {
+      await stockQueue.add(
+        { symbol },
+        {
+          attempts: 3,
+          backoff: 5000
+        }
+      );
+
+      console.log(`Job added for ${symbol}`);
+    }
+
+  } catch (error) {
+    console.error('Cron Error:', error.message);
+  }
+});
